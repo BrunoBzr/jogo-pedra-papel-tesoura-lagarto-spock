b8bf078c0c08bc36e334ae19c65f93923e87f55b
Bruno Pereira <81038402+BrunoBzr@users.noreply.github.com>
2024-08-07 11:47:02 -0300

    menu inicial
    Foi criado o menu inicial no qual pergunta para o jogador se ele quer jogar contra a maquina.

--- jogo.c
+++ jogo.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+// \n
+
+/*
+Regras: "tesoura corta papel,
+		papel cobre pedra,
+		pedra esmaga lagarto, 
+		agarto envenena Spock,
+		Spock dobra tesoura,
+		tesoura decapita lagarto,
+		lagarto come papel,
+		papel refuta Spock,
+		Spock vaporiza pedra
+		e, como sempre, pedra esmaga tesoura”.
+*/
 
 void main(){
 	char nome[100];
-	int i, escolha_jogador, escolha_maquina, qtd_rodadas;
+	int i, escolha_jogador, escolha_maquina, qtd_rodadas, jogar = 1;
 	int vitorias = 0, derrotas = 0, empates = 0;
-	printf("----- PEDRA PAPEL TESOURA LAGARTO SPOCK -----");
+
+	while(jogar != 0){
+		system("cls");
+		fflush(stdin);
+		printf("----- PEDRA PAPEL TESOURA LAGARTO SPOCK -----");
+		printf("\n\nVoce deseja jogar contra a maquina?");
+		printf("\n\n1 - Jogar\n2 - Ler regras\n0 - Sair\n>> ");
+		scanf("%d",&jogar);
+		switch(jogar)
+		{
+		case 1:
+			system("cls");
+			fflush(stdin);
+			
+			printf("Informe o nome do jogador: ");
+			gets(nome);
+			system("PAUSE");
+			break;
+		case 2:
+			system("cls");
+			printf("----- REGRAS ------\n\n");
+			system("PAUSE");
+			break;
+		case 0:
+			break;
+		default:
+			system("cls");
+			printf("Opcao invalida!\n\n");
+			system("PAUSE");
+		}
+	}
 	
 }
