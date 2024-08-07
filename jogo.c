c29467880647c7a9da7d63333da7ca7d42597a9b
Bruno Pereira <81038402+BrunoBzr@users.noreply.github.com>
2024-08-07 11:12:10 -0300

    Create jogo.c

---
+++ jogo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+	char nome[100];
+	int i, escolha_jogador, escolha_maquina, qtd_rodadas;
+	int vitorias = 0, derrotas = 0, empates = 0;
+	printf("----- PEDRA PAPEL TESOURA LAGARTO SPOCK -----");
+	
+}
